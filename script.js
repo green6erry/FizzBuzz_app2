720ae3d957c9280c81ffa5db7859d558c4465796
green6erry <green6erry@gmail.com>
2016-03-21 12:28:31 -0400

    Yiss
    I did it.

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
-for (var i = 1; i <=100; i++) {
-  var value;
-  
-    if (i%3 === 0 && i%5 === 0) {
-        value = "FizzBuzz";
-    }
-    else if (i%3 === 0 && i%5 !== 0) {
-        value = "Fizz";
-    }
-    else if (i%3 !== 0 && i%5 === 0) {
-       value = "Buzz";
-    }
-    else {
-        value=i;
-    }
+
+window.onload = function() {
+    var value="";
     
-    document.write('<li>'+value+'</li>');
+    
+    function fizzLoop() {
+        var i = prompt("Enter a number between 1 and 100");
+        console.log(i);
+    
+    if (i%1 === 0 && i > 0 && i < 100) {
+
+        for (i; i <=100; i++) {
+  
+            if (i%3 === 0 && i%5 === 0) {
+            value += "<li>FizzBuzz</li>";
+            }
+            else if (i%3 === 0 && i%5 !== 0) {
+            value += "<li>Fizz</li>";
+            }
+            else if (i%3 !== 0 && i%5 === 0) {
+           value += "<li>Buzz</li>";
+            }
+            else if (i%3 !== 0 && i%5 !== 0) {
+            value += '<li>'+i+'</li>';
+            }
+            else {
+            value += '<li></li>';
+            }
+        }
+        document.getElementById('numList').innerHTML = value;
+    }
+
+    else if (i >= 100) {
+        alert("Oops! That number is above 100. Please enter a number somewhere in between 1 and 100.");
+        return fizzLoop();
+    }
+
+    else if (i < 1) {
+        alert("Oops! That number below 1. Please enter a number somewhere in between 1 and 100.");
+        return fizzLoop();
+    }
+
+    else if (i%1 !== 0 && parseInt(i)%1 === 0){
+        alert("Oops! You're not wrong; that is a number between 1 and 100. But we're looking for an integer.");
+        return fizzLoop();
+    }
+
+    else {
+        console.log(parseInt(i));
+        alert("That has an alphabet character in it. Please just put in an integer.");
+        return fizzLoop();
+    }
+}
+fizzLoop();
+
+
+    
+
+
+
+
 
 }
+
+
